0a7f81f9a3ad1836535a002bc860da9c6c4b517c
cjburkey01 <cjburkey01@gmail.com>
2016-04-09 16:59:04 -0400

    v1.0.0

--- Forge 1.9 JSon Generator/src/com/cjburkey/forge/json/Generator.java
+++ Forge 1.9 JSon Generator/src/com/cjburkey/forge/json/Generator.java
@@ -2,7 +2,6 @@ package com.cjburkey.forge.json;
 
 import java.util.ArrayList;
 import java.util.List;
-import javafx.scene.control.TextField;
 
 public class Generator {
 	
@@ -23,22 +22,7 @@ public class Generator {
 	public static final ArrayList<Generator> generators = new ArrayList<Generator>();
 	
 	public static final void registerGenerators() {
-		//--ITEM--//
-			TextField itemModid = new TextField();
-			TextField itemUnlocalized = new TextField();
-			/*generators.add(new Generator("Simple Item", new Node[] { itemModid, itemUnlocalized }) {
-				public void extra() {
-					itemModid.setPromptText("Mod ID");
-					itemUnlocalized.setPromptText("Item Unlocalized Name");
-				}
-				public String generate() {
-					return "{\n\t\"parent\": \"item/generated\",\n\t\"textures\": {\n\t\t\"layer0\": \"" + itemModid.getText() + ":items/" + itemUnlocalized.getText() + "\"\n\t}\n}";
-				}
-				public String getFileName() {
-					return itemUnlocalized.getText();
-				}
-			});*/
-			generators.addAll(Search.loadGenerators());
+		generators.addAll(Search.loadGenerators());
 		
 	}
 	
